b80585fbdd8269a2c5e92b3217a9acfeb8321813
LucySc <19lucias@gatewayhigh.net>
2016-03-03 17:52:49 -0800

    worked more on my elf on the shelf

--- GameDev/state0.js
+++ GameDev/state0.js
@@ -24,13 +24,21 @@ demo.state0.prototype = {
 
 	update: function(){
         if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-            adam.x = adam.x + speed;   
+            adam.x = adam.x + speed;  
+            adam.animations.play('walk', 20, true);
         }
-        if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+       else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
             adam.x = adam.x - speed;
+            adam.animations.play('walk', 20, true);
+        }
+        else{
+            adam.animations.stop('walk');
         }
         if (game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-            adam.y = adam.y - speed;
+            adam.y = adam.y-speed;
+            if(adam.y < 400){
+                adam.y =400;
+            }
         }
         if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
             adam.y = adam.y + speed;
